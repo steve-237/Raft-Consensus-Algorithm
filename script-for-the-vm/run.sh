ac78de8e67dca027a1476abf3841f7db73b57e1b
mo81kufi <steve.pegouen@fau.de>
2024-05-15 10:48:25 +0200

    add code to fix network prroblem in the VM

--- script-for-the-vm/run.sh
+++ script-for-the-vm/run.sh
@@ -18,6 +18,12 @@ ip tuntap add tapgh${instance_number} mode tap user $(whoami)
 ip link set tapgh${instance_number} up
 ip link set tapgh${instance_number} master br0
 
+iptables -N BR0
+iptables -A FORWARD -j BR0
+iptables -A BR0 -i br0 -j ACCEPT
+iptables -A BR0 -o br0 -j ACCEPT
+iptables -A BR0 -j RETURN
+
 sudo ./launch-qemu.sh -hda "$hda" -cdrom user-data.img -default-network"${instance_number}" -sev-snp -smp 16 -mem 8192 # secure with sev-snp
 # sudo ./launch-qemu.sh -hda sev-guest.qcow2 -cdrom user-data.img -default-network -sev-es # secure but only encrypted state
 # sudo ./launch-qemu.sh -hda sev-guest.qcow2 -cdrom user-data.img -default-network  # start the vm without any protection
